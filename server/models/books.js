1e75a95eb4b649e1ee07773d3e32a6b86622a63d
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2021-10-15 08:10:33 +0300

    create server with mongo and moongose

---
+++ server/models/books.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+//  mongoose.set("useFindAndModify", false);
+
+const url = process.env.MONGO_URI;
+
+// console.log("connecting to", url);
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const booksSchema = new mongoose.Schema({
+  author: String,
+  name: String,
+  price: Number,
+  year: Number,
+  image: String,
+});
+
+booksSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Book", booksSchema);
